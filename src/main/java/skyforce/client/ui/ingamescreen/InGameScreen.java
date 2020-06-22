52cd04835c891da2684b476c92e1598bc38989b9
The Chinh <chinh1998@gmail.com>
2020-06-22 10:08:09 +0700

    Update InGameScreen.java

--- src/main/java/skyforce/client/ui/ingamescreen/InGameScreen.java
+++ src/main/java/skyforce/client/ui/ingamescreen/InGameScreen.java
@@ -134,7 +134,7 @@ public class InGameScreen extends JPanel implements KeyListener {
                     player.getName(),
                     player.getScore(),
                     player.getHealth()
-            ), Constants.INGAME_PADDING_START + 100 +  (width + 10), Constants.INGAME_PADDING_TOP + 20);
+            ), Constants.INGAME_PADDING_START + 300 +  (width + 10), Constants.INGAME_PADDING_TOP + 35);
         }
 
         buffer.show();
